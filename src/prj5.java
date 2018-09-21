606e98d5bae61f429652e1b0ade7b54ce5890d32
Jatin Sahoo <32974378+jatinsahoo37@users.noreply.github.com>
2018-09-21 20:40:48 +0530

    Update prj5.java

--- src/prj5.java
+++ src/prj5.java
@@ -3,26 +3,37 @@ public class prj5
 {
 	public static void main(String[] args) 
 	{
-		int res = 1;
-		
-		for(int i = 2; i<=20; i++)
-		{
-			res = LCM(i, res);
-		}
-		System.out.println("smallest multiple: "+res);
-	}
+        System.out.println(findSmallestMultiple(20));
+    }
+    
+    
+    public static long findSmallestMultiple(int n) 
+    {
+        for (long i = n; i <= factorial(n); i += n) 
+        {
+            if (isMultiple(i, n)) 
+            	return i;
+        }
+        return -1;
+    }
+     
+    public static boolean isMultiple(long x, int n) 
+    {
+        for (int i = 1; i < n; i++) 
+        {
+            if (x % i != 0) 
+            	return false;
+        }
+        return true;
+    }
+    
+    public static long factorial (long n) {
+        if (n > 1) 
+        	return n * factorial(n - 1);
+        else if (n >= 0) 
+        	return 1;
+        else 
+        	return -1;
 
-	private static int LCM(int i, int res) 
-	{
-		
-		return i * res / HCF(i, res);
-	}
-
-	private static int HCF(int i, int res) {
-		
-		if(res == 0)
-			return i;
-		else
-			return HCF( res, i % res);
 	}
 }
